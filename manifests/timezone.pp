590f51193cac5343a9a81464ffec03d693f7fe82
Joey Piccola <joey@joeypiccola.com>
2016-10-03 16:47:02 -0600

    unquoted powershell

--- manifests/timezone.pp
+++ manifests/timezone.pp
@@ -4,7 +4,7 @@ class jpi_base_win::timezone {
   exec { 'set-timezone':
     command  => 'tzutil /s “Mountain Standard Time',
     unless   => 'if (Get-WmiObject -class win32_timezone -Filter "Caption=\'(UTC-07:00) Mountain Time (US & Canada)\'") { exit 1 }',
-    provider => 'powershell',
+    provider => powershell,
   }
 
 }

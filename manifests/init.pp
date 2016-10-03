28cca9a64dd6b60f34eba09dbb8b1412233d1771
Joey Piccola <joey@joeypiccola.com>
2016-10-03 13:29:58 -0600

    new pck and srv pp called form init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,7 @@
 class jpi_base_win {
 
   include chocolatey
+  include ::jpi_base_win::packages
+  include ::jpi_base_win::services
   
-  service { 'BITS':
-      ensure => 'running'
-  }
 }
---
+++ manifests/services.pp
@@ -0,0 +1,3 @@
+service { 'BITS':
+  ensure => 'stopped'
+}

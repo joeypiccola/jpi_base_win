7f49890a9460dc6d9b43cebb4fe435ed099bb608
Joey Piccola <joey@joeypiccola.com>
2016-10-03 16:35:39 -0600

    added timezone support

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,5 +4,6 @@ class jpi_base_win {
   include chocolatey
   include ::jpi_base_win::packages
   include ::jpi_base_win::services
+  include ::jpi_base_win::timezone
 
 }
---
+++ manifests/timezone.pp
@@ -0,0 +1,10 @@
+# manage timezone with powershell
+class jpi_base_win::timezone {
+
+  exec { 'set-timezone':
+    command  => 'tzutil /s “Mountain Standard Time'
+    unless   => 'if (Get-WmiObject -class win32_timezone -Filter "Caption=\'(UTC-07:00) Mountain Time (US & Canada)\'") { exit 1 }'
+    provider => 'powershell'
+  }
+
+}

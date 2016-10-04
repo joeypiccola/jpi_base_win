2fe1e911b8a475c6bbf5963f36f80bd16a67ac16
Joey Piccola <joey@joeypiccola.com>
2016-10-04 10:12:35 -0600

    fixed timezone.pp logic added !

--- manifests/timezone.pp
+++ manifests/timezone.pp
@@ -2,8 +2,8 @@
 class jpi_base_win::timezone {
 
   exec { 'set-timezone':
-    command  => 'tzutil /s “Mountain Standard Time"',
-    unless   => 'if (Get-WmiObject -class win32_timezone -Filter "Caption=\'(UTC-07:00) Mountain Time (US & Canada)\'") { exit 1 }',
+    command  => 'tzutil /s "Mountain Standard Time"',
+    unless   => 'if (!(Get-WmiObject -class win32_timezone -Filter "StandardName=\'Mountain Standard Time\'")) { exit 1 }',
     provider => powershell,
   }
 

8220bd3aef955d2f44a478e4df3e980899ad3bc9
Joey Piccola <joey@joeypiccola.com>
2016-10-11 12:34:06 -0600

    refit array params

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,15 +2,8 @@
 class jpi_base_win::params {
 
   $timezone = 'Mountain Standard Time'
-  $package = [
-    'notepadplusplus',
-    'atom', 
-    'sublimetext3',
-  ]
+  $package = ['notepadplusplus','atom','sublimetext3']
   $interfacealias = 'Ethernet'
-  $dnsservers = [
-    '10.0.3.21',
-    '8.8.8.8', 
-    '88.4.4',
-  ]
+  $dnsservers = ['10.0.3.21','8.8.8.8','8.8.4.4']
+  
 }
